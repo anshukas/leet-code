c7605ed9f2589c14cac7e1eeb3c6b46e8133d621
Anshu Kumar <anshukansakar@gmail.com>
2022-03-05 15:43:27 +0530

    Time: 76 ms (26.33%), Space: 45.2 MB (32.12%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // brute force Algo
+        for(int i=0; i < nums.length - 1; i++) {
+            int firstNumber = nums[i];
+            for(int j = i+1; j< nums.length; j++) {
+                int secondNumber = nums[j];
+                int currentSum = firstNumber + secondNumber;
+                if(currentSum == target) {
+                   return new int[] {i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
