56e1bcba7a27f2f97d7ab32dc38c678c2c972115
Anshu Kumar <anshukansakar@gmail.com>
2022-06-08 00:50:30 +0530

    Time: 9 ms (83.94%), Space: 50.8 MB (32.15%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.java
+++ 707-design-linked-list/707-design-linked-list.java
@@ -1,56 +1,130 @@
 class MyLinkedList {
+    class Node {
+        int data;
+        Node next;
+        
+        public Node(int x){
+            this.data = x;
+            this.next = null;
+        }
+    }
+    
+    Node head;
+    Node tail;
     int size;
-    ListNode head;
 
     public MyLinkedList() {
-        size =0;
-        head = new ListNode(0);
+        this.head = null;
+		this.tail = null;
+		this.size = 0;
     }
     
     public int get(int index) {
-        if(index < 0 || index >= size)
-            return -1;
-        ListNode curr = head;
-   
-    for(int i = 0; i < index + 1; ++i) curr = curr.next;
-        return curr.val;
+        // Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        if(index < 0 || index >= this.size) return -1;
+        
+        Node curr = this.head;  
+        while(index-- > 0){
+            curr = curr.next;
+        }
+        return curr.data;
     }
     
     public void addAtHead(int val) {
-         addAtIndex(0, val);
+         Node node = new Node(val);
+         if (this.size == 0) {
+            this.head = node;
+            this.tail = node;
+         } else {
+             node.next = this.head;
+             this.head = node;
+         }
+        this.size++;
     }
     
     public void addAtTail(int val) {
-         addAtIndex(size, val);
+        Node node = new Node(val);
+        if (this.size == 0) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          this.tail.next = node;
+          node.next = null;
+          this.tail = node;
+        }
+        this.size++;
     }
     
     public void addAtIndex(int index, int val) {
-        if (index > size) return;
-        
-         if (index < 0) index = 0;
-         ++size;
-        
-        ListNode pred = head;
-        for(int i = 0; i < index; ++i) 
-            pred = pred.next;
-        
-         ListNode toAdd = new ListNode(val);
-         toAdd.next = pred.next;
-         pred.next = toAdd;
+        if (index < 0 || index > this.size)
+          return;
+
+        if (index == 0) {
+          addAtHead(val);
+        } else if (index == this.size) {
+          addAtTail(val);
+        } else {
+          Node prev = getNodeAt(index - 1);
+          Node forw = prev.next;
+          Node curr = new Node(val);
+          prev.next = curr;
+          curr.next = forw;
+          this.size++;
+        }
+    }
+    public void deleteFirst() {
+        if (this.size == 0)
+          return;
+        else if (this.size == 1) {
+          this.head = null;
+          this.tail = null;
+        } else {
+          Node curr = this.head;
+          Node forw = curr.next;
+          curr.next = null;
+          this.head = forw;
+        }
+        this.size--;
+  }
+    public void deleteLast() {
+        if (this.size == 0)
+          return;
+        else if (this.size == 1) {
+          this.head = null;
+          this.tail = null;
+        } else {
+          Node secondLast = getNodeAt(this.size - 2);
+          secondLast.next = null;
+          this.tail = secondLast;
+        }
+        this.size--;
+  }
+    public void deleteAtIndex(int index) {
+         if (index < 0 || index >= this.size)
+          return;
+
+        if (index == 0) {
+          deleteFirst();
+        } else if (index == this.size - 1) {
+          deleteLast();
+        } else {
+          Node prev = getNodeAt(index - 1);
+          Node curr = prev.next;
+          Node forw = prev.next.next;
+          prev.next = forw;
+          curr.next = null;
+          this.size--;
+         }
     }
     
-    public void deleteAtIndex(int index) {
-        // if the index is invalid, do nothing
-        if (index < 0 || index >= size) return;
-
-        size--;
-        // find predecessor of the node to be deleted
-        ListNode pred = head;
-        for(int i = 0; i < index; ++i) pred = pred.next;
-
-        // delete pred.next 
-        pred.next = pred.next.next;
+public Node getNodeAt(int index) {
+    Node curr = head;
+    while (index--> 0) {
+      curr = curr.next;
     }
+
+    return curr;
+  }
 }
 
 /**
