6e147b29a246b53c5bab47ef76c511422e29f1aa
Anshu Kumar <anshukansakar@gmail.com>
2023-02-11 06:22:57 +0530

    Time: 7 ms (82.27%), Space: 45.7 MB (52.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,22 @@
 class Solution {
-
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        if (strs.length == 0) return res;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            char[] hash = new char[26];
-            for (char c : s.toCharArray()) {
-                hash[c - 'a']++;
-            }
-            String key = new String(hash);
-            map.computeIfAbsent(key, k -> new ArrayList<>());
+        // map: {key : list}, K is sorted and every time sort str from array
+        // compare with Key and add in map's list.
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        // itrate array
+        for (String s: strs){
+            char[] charArr = s.toCharArray();
+            Arrays.sort(charArr);
+            String key = String.valueOf(charArr);
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            
+            // add current string in map too.
             map.get(key).add(s);
+            
         }
-        res.addAll(map.values());
-        return res;
+        return new ArrayList(map.values());
     }
 }
