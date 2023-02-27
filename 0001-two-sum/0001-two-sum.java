9cfd06e7860342b85b953e5332c657e07bc5ba2d
Anshu Kumar <anshukansakar@gmail.com>
2023-02-27 06:17:06 +0530

    Time: 2 ms (82.97%), Space: 42.7 MB (40.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // use map where we keep (diff,i)
-        // if we find while itrating then create array and return index.
-        
-        Map<Integer,Integer> map = new HashMap<>();
+        //nums = [2,7,11,15], target = 9
+        // diff = 9-2=7
+        // map: {2,0}
+        // i=1
+            //9-7=2
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i< nums.length; i++){
             int diff = target - nums[i];
-            
             if(map.containsKey(diff)){
                 return new int[] {map.get(diff), i};
             }
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
-        return new int[] {};
+        return null;
     }
 }
